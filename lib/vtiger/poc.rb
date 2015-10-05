92fd2fa862b20a993a3da514ba627892ac55a612
Augusto Pedraza <augusto.pedraza08@gmail.com>
2015-10-05 13:38:07 -0300

    Added a basic proof of concept for VTiger API

--- lib/vtiger/poc.rb
+++ lib/vtiger/poc.rb
@@ -1,16 +1,35 @@
-require 'httparty'
+require 'rest-client'
+require 'digest'
 
 module VTiger
+  class LoginError < StandardError; end
   class Poc
-    include HTTParty
-
-    def initialize(user_key)
-      options = { query: { 'accessKey' => user_key, username: 'admin', operation: 'login' } }
-
-      r = self.class.post('hedra.crm.netsac.com.br/webservice.php', options)
-      byebug
+    def initialize(username, user_key)
+      @base_uri = 'http://hedra.crm.netsac.com.br/vtigercrm/webservice.php'
+      @session_id = login(username, user_key)
     end
+
+    def list_objs
+      params = {operation: 'listtypes', sessionName: @session_id}
+      JSON.parse(RestClient.get(@base_uri, params: params))
+    end
+
+    private
+      def login(username, user_key)
+        form = { operation: 'login', username: username, accessKey: create_access_key(username, user_key) }
+        JSON.parse(RestClient.post(@base_uri, form))['result']['sessionName']
+      end
+
+      def token_for(username)
+        opts = { params: { operation: 'getchallenge', username: username }, :accept => :json, :content_type => :json }
+        res = JSON.parse(RestClient.get(@base_uri, opts))
+        raise LoginError, 'Error to get token for log in.' if res["success"] != true || res['result']['token'].blank?
+
+        res['result']['token']
+      end
+
+      def create_access_key(username, user_key)
+        Digest::MD5.hexdigest(token_for(username) + user_key)
+      end
   end
 end
-
-HTTParty.get('http://hedra.crm.netsac.com.br/webservice.php?operation=getchallenge&username=admin')
